8ae2a4a84646ac11240321de4e73d0a211b4e5ff
James Adogo <dbase@pergersys.com>
2026-05-12 15:39:07 +0000

    Turning secure value of cookie to true when environment is production

--- Controllers/User.js
+++ Controllers/User.js
@@ -86,14 +86,14 @@ export const loginRestaurant = async(req , res)=>{
 
         res.cookie("accessToken",accessToken,{
             httpOnly:true,
-            secure: process.env.NODE_ENV,
+            secure: process.env.NODE_ENV=="production",
             maxAge: 7*24*60*60*1000,
             sameSite:"strict"
             
         })
         res.cookie("refreshToken",refreshToken,{
             httpOnly:true,
-            secure: process.env.NODE_ENV,
+            secure: process.env.NODE_ENV=="production",
             maxAge: 15*60*1000,
             sameSite:"strict"
             
