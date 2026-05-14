3fbdf710b721b5a406601663fa919953c29a05fc
James Adogo <dbase@pergersys.com>
2026-05-14 15:34:46 +0000

    Added enpoints for restaurant menu

--- Utils/GenerateAccessToken.js
+++ Utils/GenerateAccessToken.js
@@ -5,7 +5,7 @@ dotenv.config()
 export const generateAccessToken = (userId)=>{
     try{
 
-        const accessToken = jwt.sign({id:userId},process.env.ACCESS_TOKEN_SECRET,{expiresIn:"15min"})
+        const accessToken = jwt.sign({id:userId},process.env.ACCESS_TOKEN_SECRET,{expiresIn:"1d"})
 
         return accessToken
 
