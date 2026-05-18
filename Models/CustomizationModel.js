36d411aed1870e19b1482a220df8d48dd4d3fcad
James Adogo <dbase@pergersys.com>
2026-05-18 16:00:29 +0000

    Added customizations for admion, fixed table number value for orders with mobile money

---
+++ Models/CustomizationModel.js
@@ -0,0 +1,43 @@
+import mongoose from "mongoose";
+
+const customizationSchema = new mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+      unique: true,
+    },
+
+    logo: {
+      type: String,
+      default: "",
+    },
+
+    motto: {
+      type: String,
+      default: "",
+      trim: true,
+    },
+
+    primaryColor: {
+      type: String,
+      default: "#0f172a",
+    },
+
+    secondaryColor: {
+      type: String,
+      default: "#ffffff",
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const CustomizationModel = mongoose.model(
+  "Customization",
+  customizationSchema
+);
+
+export default CustomizationModel;
--- Route/authRoute.js
+++ Route/authRoute.js
@@ -1,5 +1,8 @@
 import express from "express"
-import { signUpRestaurant, loginRestaurant, refreshToken } from "../Controllers/User.js"
+import { signUpRestaurant, loginRestaurant, refreshToken, changePassword, updateCustomization, updateRestaurantDetails, getAllDetails, getAllPublicDetails } from "../Controllers/User.js"
+import { authenticateRestaurant, checkRole } from "../Middleware/auth.js"
+import { uploadMenuStorage } from "../Middleware/UploadFile.js"
+
 
 
 export const authRoute = express.Router()
@@ -7,3 +10,8 @@ export const authRoute = express.Router()
 authRoute.post("/sign-up",signUpRestaurant)
 authRoute.post("/login",loginRestaurant)
 authRoute.post("/refresh-access-token", refreshToken)
+authRoute.put("/change-password",authenticateRestaurant,checkRole,changePassword)
+authRoute.put("/update-customizations",authenticateRestaurant,checkRole,uploadMenuStorage.single("image"),updateCustomization)
+authRoute.put("/update-details",authenticateRestaurant,checkRole,updateRestaurantDetails)
+authRoute.get("/all-details",authenticateRestaurant,checkRole,getAllDetails)
+authRoute.get("/all-details/:id",authenticateRestaurant,checkRole,getAllPublicDetails)
